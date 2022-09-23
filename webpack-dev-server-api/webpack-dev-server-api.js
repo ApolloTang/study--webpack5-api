a0af5d883a4974cc8dcb283798d131e171e5687a
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-09-23 00:59:47 -0400

    working: webpack dev server api

---
+++ webpack-dev-server-api/webpack-dev-server-api.js
@@ -0,0 +1,19 @@
+const CreateWebpackCompiler = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+const {getConfig} = require("./webpack/webpack.config.js")
+
+const webpackConfig = getConfig('development')
+console.dir(webpackConfig, {depth: 8})
+
+const webpackCompiler = CreateWebpackCompiler(webpackConfig);
+
+const devServerOptions = { ...webpackConfig.devServer, open: true };
+const server = new WebpackDevServer(devServerOptions, webpackCompiler);
+
+const runServer = async () => {
+  console.log('Starting server...');
+  await server.start();
+};
+
+runServer();
---
+++ webpack-dev-server-api/webpack/webpack.config.js
@@ -0,0 +1,45 @@
+const { merge } = require('webpack-merge')
+
+const part_page = require('./webpack-part--page.js')
+const part_loadCss = require('./webpack-part--load-css.js')
+const part_extractCss = require('./webpack-part--extract-css.js')
+
+
+const cssloader_postcss = require('./webpack-part--cssloader--postcss.js')
+
+const commonConfig = merge([
+  {
+    entry: [ './src/main.js' ]
+  },
+  part_page({title:'demo'}),
+])
+
+
+const productionConfig = merge([
+  part_extractCss({
+    loaders: [cssloader_postcss()]
+  })
+])
+
+
+const developmentConfig = merge([
+  part_loadCss({
+    loaders: [cssloader_postcss()]
+  })
+])
+
+
+const getConfig = (mode, debug=false) => {
+  switch (mode) {
+    case 'production':
+      return merge(commonConfig, productionConfig, {mode: !debug ? mode : 'none'})
+    case 'development':
+      return merge(commonConfig, developmentConfig, {mode})
+    default:
+      throw new Error(`Trying to use an unknow mode, ${mode}`)
+  }
+}
+
+module.exports = {
+  getConfig
+}
